4cdc5d77712023ff858e028828a4f1ef87563ae3
Ionut <ionutputinicabz@gmail.com>
2019-03-09 21:25:42 +0100

    Added DisplayMeals activity

---
+++ app/src/main/java/com/calCounterapplicaton/app/DisplayMealsActivity.java
@@ -0,0 +1,13 @@
+package com.calCounterapplicaton.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_meals);
+    }
+}
