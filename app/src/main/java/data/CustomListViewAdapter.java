dba648d0c8b38d7999cc7106e6169f17b2ed83b0
Ionut <ionutputinicabz@gmail.com>
2019-03-12 11:06:10 +0100

    Added fields and constructor for CustomListViewAda

--- app/src/main/java/data/CustomListViewAdapter.java
+++ app/src/main/java/data/CustomListViewAdapter.java
@@ -1,4 +1,22 @@
 package data;
+import android.app.Activity;
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
 
-public class CustomListViewAdapter {
+import model.Meal;
+
+public class CustomListViewAdapter extends ArrayAdapter<Meal> {
+    private int layoutResource;
+    private Activity activity;
+    private ArrayList<Meal> mealList=new ArrayList<>();
+    public CustomListViewAdapter(Activity act, int resource, ArrayList<Meal> data) {
+        super(act, resource, data);
+        layoutResource = resource;
+        activity=act;
+        mealList=data;
+        notifyDataSetChanged();
+    }
+
+    
 }
