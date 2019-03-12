a45c4263e792832129f4c18dbefcdfc991530923
Ionut <ionutputinicabz@gmail.com>
2019-03-12 11:30:29 +0100

    Logic in DbsHandler for onCreate and onUpgrade

--- app/src/main/java/data/DatabaseHandler.java
+++ app/src/main/java/data/DatabaseHandler.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
 import data.Constants;
+import model.*;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private final ArrayList<Meal> mealList= new ArrayList<>();
+
 
    public DatabaseHandler(Context context)
    {
@@ -15,11 +21,20 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+       //creating the table
+        String CREATE_TABLE = "CREATE TABLE " + Constants.TABLE_NAME + "("
+                + Constants.KEY_ID + " INTEGER PRIMARY KEY, " + Constants.MEAL_NAME +
+                " TEXT, " + Constants.MEAL_CALORIES_NAME + " INT, " + Constants.DATE_NAME + " LONG);";
+
+        db.execSQL(CREATE_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
 
+    //create a new  table in case there is a new one
+        onCreate(db);
     }
 }
