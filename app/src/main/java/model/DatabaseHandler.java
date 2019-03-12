cf11719a6f68362631f2a7b5f9cb7cbbb2bf4977
Ionut <ionutputinicabz@gmail.com>
2019-03-12 10:53:11 +0100

    Changes to DatabaseHandler class

--- app/src/main/java/model/DatabaseHandler.java
+++ app/src/main/java/model/DatabaseHandler.java
@@ -1,4 +1,25 @@
 package model;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import data.Constants;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+   public DatabaseHandler(Context context)
+   {
+       super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+   }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
