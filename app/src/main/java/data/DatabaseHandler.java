1eeeaadd0efbe3379d37832f164b96fc780efdae
Ionut <ionutputinicabz@gmail.com>
2019-03-12 12:27:55 +0100

    Added a method that gets all the meals from the db

--- app/src/main/java/data/DatabaseHandler.java
+++ app/src/main/java/data/DatabaseHandler.java
@@ -16,17 +16,16 @@ import model.*;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    private final ArrayList<Meal> mealList= new ArrayList<>();
+    private final ArrayList<Meal> mealList = new ArrayList<>();
 
 
-   public DatabaseHandler(Context context)
-   {
-       super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
-   }
+    public DatabaseHandler(Context context) {
+        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-       //creating the table
+        //creating the table
         String CREATE_TABLE = "CREATE TABLE " + Constants.TABLE_NAME + "("
                 + Constants.KEY_ID + " INTEGER PRIMARY KEY, " + Constants.MEAL_NAME +
                 " TEXT, " + Constants.MEAL_CALORIES_NAME + " INT, " + Constants.DATE_NAME + " LONG);";
@@ -37,35 +36,33 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
 
-    //create a new  table in case there is a new one
+        //create a new  table in case there is a new one
         onCreate(db);
     }
+
     //get total number of itemes that the user has saved within the application
-    public int getTotalItems()
-    {
-    int totalItems=0;
-    String query= "SELECT * FROM "+ Constants.TABLE_NAME;
-    SQLiteDatabase dba= this.getReadableDatabase();
-    Cursor cursor= dba.rawQuery(query,null);
-    totalItems=cursor.getCount();
-    cursor.close();
+    public int getTotalItems() {
+        int totalItems = 0;
+        String query = "SELECT * FROM " + Constants.TABLE_NAME;
+        SQLiteDatabase dba = this.getReadableDatabase();
+        Cursor cursor = dba.rawQuery(query, null);
+        totalItems = cursor.getCount();
+        cursor.close();
 
 
-    return totalItems;
+        return totalItems;
     }
 
     //return the total calorie intake for the day
-    public int totalCalories()
-    {
-        int cals=0;
-        SQLiteDatabase dba= this.getReadableDatabase();
-        String query= "SELECT SUM( " + Constants.MEAL_CALORIES_NAME+ " )" + "FROM " + Constants.TABLE_NAME;
-        Cursor cursor= dba.rawQuery(query,null);
-        if(cursor.moveToFirst())
-        {
-            cals=cursor.getInt(0);
+    public int totalCalories() {
+        int cals = 0;
+        SQLiteDatabase dba = this.getReadableDatabase();
+        String query = "SELECT SUM( " + Constants.MEAL_CALORIES_NAME + " )" + "FROM " + Constants.TABLE_NAME;
+        Cursor cursor = dba.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            cals = cursor.getInt(0);
         }
         cursor.close();
         dba.close();
@@ -74,9 +71,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //delete a meal from the list
     //the id parsing is used to find the item
-    public void deleteMeal(int id)
-    {
-        SQLiteDatabase dba= this.getWritableDatabase();
+    public void deleteMeal(int id) {
+        SQLiteDatabase dba = this.getWritableDatabase();
         //the table is passed in
         dba.delete(Constants.TABLE_NAME, Constants.KEY_ID + " = ?",
                 new String[]{String.valueOf(id)});
@@ -84,8 +80,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         dba.close();
     }
 
-    public void addMeal(Meal meal)
-    {
+    public void addMeal(Meal meal) {
         SQLiteDatabase dba = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -103,5 +98,40 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     //get all meals from the database
-    
+    public ArrayList<Meal> getMeals() {
+
+        mealList.clear();
+
+        SQLiteDatabase dba = this.getReadableDatabase();
+
+        Cursor cursor = dba.query(Constants.TABLE_NAME,
+                new String[]{Constants.KEY_ID, Constants.MEAL_NAME, Constants.MEAL_CALORIES_NAME,
+                        Constants.DATE_NAME}, null, null, null, null, Constants.DATE_NAME + " DESC ");
+
+        //loop through...
+        if (cursor.moveToFirst()) {
+            do {
+
+                Meal meal = new Meal();
+                meal.setMealName(cursor.getString(cursor.getColumnIndex(Constants.MEAL_NAME)));
+                meal.setCalories(cursor.getInt(cursor.getColumnIndex(Constants.MEAL_CALORIES_NAME)));
+                meal.setMealId(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ID)));
+
+
+                DateFormat dateFormat = DateFormat.getDateInstance();
+                String date = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.DATE_NAME))).getTime());
+
+                meal.setRecordDate(date);
+
+                mealList.add(meal);
+            } while (cursor.moveToNext());
+
+
+        }
+
+        cursor.close();
+        dba.close();
+
+        return mealList;
+    }
 }
