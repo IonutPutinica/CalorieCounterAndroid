f192ac3c67d42b1dc97a98157a004c7765ec1243
Ionut <ionutputinicabz@gmail.com>
2019-03-12 21:16:32 +0100

    Added parameters for main activity

--- app/src/main/java/com/calCounterapplicaton/app/MainActivity.java
+++ app/src/main/java/com/calCounterapplicaton/app/MainActivity.java
@@ -1,9 +1,25 @@
 package com.calCounterapplicaton.app;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import model.*;
+
+import data.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText foodName, foodCals;
+    private Button submitButton;
+    private DatabaseHandler dba;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
