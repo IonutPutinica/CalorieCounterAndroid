11924d3792788a93302020c3bdf8baaddfb1b7d0
Ionut <ionutputinicabz@gmail.com>
2019-03-13 14:07:42 +0100

    Added formatNumber method in Utils

--- app/src/main/java/util/Utils.java
+++ app/src/main/java/util/Utils.java
@@ -1,10 +1,14 @@
 package util;
+import java.text.DecimalFormat;
 
 public class Utils {
 
     public static String formatNumber(int value)
     {
-        
+        //this is how the numbers are going to be displayed
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+        String formatted = formatter.format(value);
+        return formatted;
     }
 
 }
