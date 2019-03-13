2954ef6979ead4bab79ffb05fd1025f25db4d378
Ionut <ionutputinicabz@gmail.com>
2019-03-13 13:46:54 +0100

    Added variables for DisplayMealsActivity

--- app/src/main/java/com/calCounterapplicaton/app/DisplayMealsActivity.java
+++ app/src/main/java/com/calCounterapplicaton/app/DisplayMealsActivity.java
@@ -1,9 +1,22 @@
 package com.calCounterapplicaton.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import data.CustomListViewAdapter;
+import data.DatabaseHandler;
+import model.Meal;
 
 public class DisplayMealsActivity extends AppCompatActivity {
+
+    private DatabaseHandler dba;
+    private ArrayList<Meal> dbMeals = new ArrayList<>();
+    private CustomListViewAdapter mealAdapter;
+    private ListView listView;
+    private Meal myMeal;
+    private TextView totalCals, totalMeals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
