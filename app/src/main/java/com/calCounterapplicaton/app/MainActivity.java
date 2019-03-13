9b5141952d369291c36e4d4e52a5c56e4d53562b
Ionut <ionutputinicabz@gmail.com>
2019-03-13 13:11:47 +0100

    Changes to MainActivity

--- app/src/main/java/com/calCounterapplicaton/app/MainActivity.java
+++ app/src/main/java/com/calCounterapplicaton/app/MainActivity.java
@@ -15,15 +15,86 @@ import model.*;
 
 import data.DatabaseHandler;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText foodName, foodCals;
+
+public class MainActivity extends ActionBarActivity {
+    private EditText mealName, mealCals;
     private Button submitButton;
     private DatabaseHandler dba;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        dba = new DatabaseHandler(MainActivity.this);
+
+        mealName = (EditText) findViewById(R.id.foodEditText);
+        mealCals = (EditText) findViewById(R.id.caloriesEditText);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                saveDataToDB();
+
+            }
+        });
+    }
+
+    private void saveDataToDB() {
+
+        Meal meal = new Meal();
+        //trimming to get rid of white spaces
+        String name = mealName.getText().toString().trim();
+        String calsString = mealCals.getText().toString().trim();
+
+        int cals = Integer.parseInt(calsString);
+
+        if (name.equals("") || calsString.equals("")) {
+
+            Toast.makeText(getApplicationContext(), "No empty fields allowed", Toast.LENGTH_LONG).show();
+
+        }else {
+
+            meal.setMealName(name);
+            meal.setCalories(cals);
+
+            dba.addMeal(meal);
+            dba.close();
+
+
+            //clear the form
+            mealName.setText("");
+            mealCals.setText("");
+
+            //take users to next screen (display all entered items)
+            startActivity(new Intent(MainActivity.this, DisplayFoodsActivity.class));
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
--- app/src/main/java/data/CustomListViewAdapter.java
+++ app/src/main/java/data/CustomListViewAdapter.java
@@ -1,18 +1,17 @@
 package data;
+
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.calCounterapplicaton.app.R;
+
 import java.util.ArrayList;
 
-
-import model.*;
+import model.Meal;
 
 public class CustomListViewAdapter extends ArrayAdapter<Meal> {
     private int layoutResource;
