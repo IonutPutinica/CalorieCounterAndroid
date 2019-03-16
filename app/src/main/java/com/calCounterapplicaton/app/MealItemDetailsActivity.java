682ccb1530f55fc911f2600e327cca700648f04c
Ionut <ionutputinicabz@gmail.com>
2019-03-16 12:07:26 +0100

    Added onCreateOptionsMenu in MealItemsDetailsActiv

--- app/src/main/java/com/calCounterapplicaton/app/MealItemDetailsActivity.java
+++ app/src/main/java/com/calCounterapplicaton/app/MealItemDetailsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -72,4 +73,11 @@ public class MealItemDetailsActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_meal_item_details, menu);
+        return true;
+    }
+
 }
