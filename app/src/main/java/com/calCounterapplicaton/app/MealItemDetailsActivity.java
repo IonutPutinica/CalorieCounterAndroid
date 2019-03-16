8a5fe60b7e2ccfcd78fbe0149a38fcfed6ba9e6d
Ionut <ionutputinicabz@gmail.com>
2019-03-16 10:52:27 +0100

    Finalized onCreate() in MealItemDetailsActivity

--- app/src/main/java/com/calCounterapplicaton/app/MealItemDetailsActivity.java
+++ app/src/main/java/com/calCounterapplicaton/app/MealItemDetailsActivity.java
@@ -1,9 +1,13 @@
 package com.calCounterapplicaton.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
+
+
+import model.Meal;
 
 public class MealItemDetailsActivity extends AppCompatActivity {
     private TextView mealName, calories, dateTaken;
@@ -13,5 +17,22 @@ public class MealItemDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_item_details);
+
+        mealName= (TextView) findViewById(R.id.detailsMeal);
+        calories= (TextView) findViewById(R.id.detailsCaloriesValue);
+        dateTaken= (TextView) findViewById(R.id.detailsDateText);
+        shareButton= (Button) findViewById(R.id.detailsShareButton);
+
+        Meal meal = (Meal) getIntent().getSerializableExtra("userObj");
+
+        mealName.setText(meal.getMealName());
+        calories.setText(String.valueOf(meal.getCalories()));
+        dateTaken.setText(meal.getRecordDate());
+
+        mealId= meal.getMealId();
+
+        calories.setTextSize(34.9f);
+        calories.setTextColor(Color.RED);
+
     }
 }
